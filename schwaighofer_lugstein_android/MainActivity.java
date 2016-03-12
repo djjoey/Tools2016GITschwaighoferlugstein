e1ee914c9f438455e42a030129a9a933aac671f0
s1510237036 <S1510237036@students.fh-hagenberg.at>
2016-03-12 15:27:12 +0100

    deleted all useless methods

--- schwaighofer_lugstein_android/MainActivity.java
+++ schwaighofer_lugstein_android/MainActivity.java
@@ -1,23 +1,3 @@
 public static void main(int args[]){
 	System.out.println("Hello World!");
 }
-
-public void thisIsANewMethod(){ 
-	int b = 999999;
-	
-	int a = 0;
-	
-	if(a < b)
-		System.out.println("wow");
-	else
-		System.out.println("How could this happen to me? I've made my mistakes - Got nowhere to run, the night goes on - As I'm fading away");
-
-	int a = 400;
-	
-	String msg = "hello world!!!";
-
-}
-
-public void thisIsAnOldMethod(){
-	System.out.println("I tried so hard, and got so far, in the end it doesnt even matter ... #toodeepforme")
-}
