04a10f6750b00dd03fdd830296eaf9033bae3f80
s1510237020 <s1510237020@students.fh-hagenberg.at>
2016-03-12 15:00:15 +0100

    added more code in thisisamethod

--- schwaighofer_lugstein_android/MainActivity.java
+++ schwaighofer_lugstein_android/MainActivity.java
@@ -6,4 +6,9 @@ public void thisIsANewMethod(){
 	int b = 999999;
 	
 	int a = 0;
+	
+	if(a < b)
+		System.out.println("wow");
+	else
+		System.out.println("How could this happen to me? I've made my mistakes - Got nowhere to run, the night goes on - As I'm fading away");
 }
