41b0027cc090961ab4bdc9df0d36bbbe0a180737
s1510237020 <s1510237020@students.fh-hagenberg.at>
2016-03-12 15:01:50 +0100

    added new method

--- schwaighofer_lugstein_android/MainActivity.java
+++ schwaighofer_lugstein_android/MainActivity.java
@@ -12,3 +12,7 @@ public void thisIsANewMethod(){
 	else
 		System.out.println("How could this happen to me? I've made my mistakes - Got nowhere to run, the night goes on - As I'm fading away");
 }
+
+public void thisIsAnOldMethod(){
+	System.out.println("I tried so hard, and got so far, in the end it doesnt even matter ... #toodeepforme")
+}
